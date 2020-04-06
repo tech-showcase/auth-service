79f1269dc3bcb9962358fa0bae663bfcb3b31bee
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-07 02:14:30 +0700

    feat(helper): add generate password func

--- go-simple-app/helper/utils.go
+++ go-simple-app/helper/utils.go
@@ -1,5 +1,14 @@
 package helper
 
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
 func Generate4CharsPassword(input string) string {
-	return ""
+	h := sha256.New()
+	h.Write([]byte(input))
+	output := fmt.Sprintf("%x", h.Sum(nil))
+
+	return output[len(output)-4:]
 }
