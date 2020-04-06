47ed8f92f100b8f90460c7384a8fcf6edefdabc9
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-07 01:47:49 +0700

    feat(presenter): add jwt private claims struct

---
+++ go-simple-app/presenter/jwt.go
@@ -0,0 +1,10 @@
+package presenter
+
+type (
+	PrivateClaims struct {
+		Name      string `json:"name"`
+		Phone     string `json:"phone"`
+		Role      string `json:"role"`
+		Timestamp string `json:"timestamp"`
+	}
+)
