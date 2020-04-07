c83178f538d418c01a8aa8e4d79c8d40a5f957d6
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-07 22:34:33 +0700

    feat(middleware): add jwt authentication middleware

---
+++ go-simple-app/middleware/auth.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-simple-app/helper"
+	"go-simple-app/presenter"
+	"go-simple-app/singleton"
+	"net/http"
+	"strings"
+)
+
+func JWTAuthenticationMiddleware(ctx *gin.Context) {
+	authHeader := presenter.AuthHeaderStruct{}
+	if err := ctx.ShouldBindHeader(&authHeader); err != nil || authHeader.Authorization == "" {
+		ctx.JSON(http.StatusBadRequest, map[string]string{"message": "request should contains authorization header"})
+		return
+	}
+
+	if !strings.Contains(authHeader.Authorization, "Bearer ") {
+		ctx.JSON(http.StatusUnauthorized, map[string]string{"message": "token should contains Bearer"})
+		return
+	}
+
+	token := strings.TrimPrefix(authHeader.Authorization, "Bearer ")
+	token = strings.TrimSpace(token)
+
+	authHelper := helper.NewAuthBlueprint()
+	claims, err := authHelper.ParseTokenWithoutKey(token)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, map[string]string{"message": "token is invalid"})
+		return
+	}
+
+	user := singleton.UsersRepo.GetUserByPhone(claims.Phone)
+	claims, err = authHelper.ParseToken(token, user.Password)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, map[string]string{"message": "token is invalid"})
+		return
+	}
+
+	ctx.Next()
+}
