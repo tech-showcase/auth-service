4b0b7709c86f9032827f34ef4418f2c685c04e87
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-07 22:35:07 +0700

    feat(api): add api to fetch active user

--- go-simple-app/api/auth.go
+++ go-simple-app/api/auth.go
@@ -3,9 +3,14 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"go-simple-app/controller"
+	"go-simple-app/middleware"
 )
 
 func RegisterAuthAPI(router *gin.Engine) {
 	router.POST("/api/register", controller.Register)
 	router.POST("/api/login", controller.Login)
+	router.Use(middleware.JWTAuthenticationMiddleware)
+	{
+		router.POST("/api/user", controller.GetActiveUser)
+	}
 }
