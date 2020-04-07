90a0a34f141101ff31ceed7ccecae3e442f4f3f3
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-07 16:18:58 +0700

    feat(repository): add user repo as user data source for domain logic

---
+++ go-simple-app/singleton/repository.go
@@ -0,0 +1,28 @@
+package singleton
+
+import "go-simple-app/model"
+
+var UsersRepo UsersRepoInterface
+
+func init() {
+	UsersRepo = NewUsersRepo()
+}
+
+type (
+	UsersRepoBlueprint struct {
+		Data model.Users
+	}
+	UsersRepoInterface interface {
+		AddOrUpdateUser(user model.User)
+	}
+)
+
+func NewUsersRepo() UsersRepoInterface {
+	var instance UsersRepoBlueprint
+
+	return &instance
+}
+
+func (instance *UsersRepoBlueprint) AddOrUpdateUser(user model.User) {
+	
+}
