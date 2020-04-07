10ab19902bac9b6c2fca0aba6dfe56f6ba85b592
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-07 22:22:35 +0700

    test(helper): add test for ParseTokenWithoutKey func
    - refactor dummy input and expected output

--- go-simple-app/helper/auth_test.go
+++ go-simple-app/helper/auth_test.go
@@ -7,14 +7,9 @@ import (
 )
 
 func TestAuthBlueprint_GenerateToken(t *testing.T) {
-	dummyPrivateClaims := presenter.PrivateClaims{
-		Name:      "dummyName",
-		Phone:     "082123456789",
-		Role:      "dummyRole",
-		Timestamp: "1257894000",
-	}
-	dummyKey := "1234"
-	expectedToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZHVtbXlOYW1lIiwicGhvbmUiOiIwODIxMjM0NTY3ODkiLCJyb2xlIjoiZHVtbXlSb2xlIiwidGltZXN0YW1wIjoiMTI1Nzg5NDAwMCJ9.Govi5TIIF7oE1W8yKlV_t81B0-a-gJJ0T2GMO-8tB68"
+	dummyPrivateClaims := getDummyClaims()
+	dummyKey := getDummyKey()
+	expectedToken := getDummyToken()
 
 	authObject := NewAuthBlueprint()
 	token, err := authObject.GenerateToken(dummyPrivateClaims, dummyKey)
@@ -27,14 +22,9 @@ func TestAuthBlueprint_GenerateToken(t *testing.T) {
 }
 
 func TestAuthBlueprint_ParseToken(t *testing.T) {
-	dummyKey := "1234"
-	dummyToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZHVtbXlOYW1lIiwicGhvbmUiOiIwODIxMjM0NTY3ODkiLCJyb2xlIjoiZHVtbXlSb2xlIiwidGltZXN0YW1wIjoiMTI1Nzg5NDAwMCJ9.Govi5TIIF7oE1W8yKlV_t81B0-a-gJJ0T2GMO-8tB68"
-	expectedPrivateClaims := presenter.PrivateClaims{
-		Name:      "dummyName",
-		Phone:     "082123456789",
-		Role:      "dummyRole",
-		Timestamp: "1257894000",
-	}
+	dummyKey := getDummyKey()
+	dummyToken := getDummyToken()
+	expectedPrivateClaims := getDummyClaims()
 
 	authObject := NewAuthBlueprint()
 	claims, err := authObject.ParseToken(dummyToken, dummyKey)
@@ -44,4 +34,38 @@ func TestAuthBlueprint_ParseToken(t *testing.T) {
 	} else if !reflect.DeepEqual(claims, expectedPrivateClaims) {
 		t.Fatal("unexpected output")
 	}
+}
+
+func TestAuthBlueprint_ParseTokenWithoutKey(t *testing.T) {
+	dummyToken := getDummyToken()
+	expectedPrivateClaims := getDummyClaims()
+
+	authObject := NewAuthBlueprint()
+	claims, err := authObject.ParseTokenWithoutKey(dummyToken)
+
+	if err != nil {
+		t.Fatal("error has occurred")
+	} else if !reflect.DeepEqual(claims, expectedPrivateClaims) {
+		t.Fatal("unexpected output")
+	}
+}
+
+func getDummyToken() string {
+	dummyToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZHVtbXlOYW1lIiwicGhvbmUiOiIwODIxMjM0NTY3ODkiLCJyb2xlIjoiZHVtbXlSb2xlIiwidGltZXN0YW1wIjoiMTI1Nzg5NDAwMCJ9.Govi5TIIF7oE1W8yKlV_t81B0-a-gJJ0T2GMO-8tB68"
+	return dummyToken
+}
+
+func getDummyKey() string {
+	dummyKey := "1234"
+	return dummyKey
+}
+
+func getDummyClaims() presenter.PrivateClaims {
+	dummyPrivateClaims := presenter.PrivateClaims{
+		Name:      "dummyName",
+		Phone:     "082123456789",
+		Role:      "dummyRole",
+		Timestamp: "1257894000",
+	}
+	return dummyPrivateClaims
 }
