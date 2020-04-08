de42e92d794f19304e73ae6bc25caf0e7455afdf
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-09 03:33:13 +0700

    fix(model): add status_code checking before updating rate
    - make sure convert_to_dollar return string or None
    - rename dollar_rate to rate_against_dollar

--- py-simple-app/model.py
+++ py-simple-app/model.py
@@ -13,18 +13,22 @@ class Resources:
 
 class Rupiah:
     def __init__(self, url):
-        self.dollar_rate = 0.0
+        self.rate_against_dollar = 0.0
         self.url = url
 
     def fetch(self):
         result = get(self.url)
         result_dict = result.json()
-        self.dollar_rate = result_dict['IDR_USD']
-        print(self.dollar_rate, result_dict)
+        if result.status_code == 200:
+            self.rate_against_dollar = result_dict['IDR_USD']
+            print('fetched rupiah rate: ', self.rate_against_dollar)
+        else:
+            error = result_dict['error']
+            print('failed to fetch rupiah rate: ', error)
 
     def convert_to_dollar(self, price_in_rupiah):
         if price_in_rupiah is None:
             return None
         else:
             price_in_rupiah = float(price_in_rupiah)
-            return self.dollar_rate * price_in_rupiah
+            return str(self.rate_against_dollar * price_in_rupiah)
