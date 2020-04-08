1cac4d3d86d76e78aa96b580a560e6d065b696be
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-09 00:15:51 +0700

    feat(model): add Rupiah class
    - add fetch method to get info periodically

--- py-simple-app/model.py
+++ py-simple-app/model.py
@@ -1,4 +1,5 @@
 from requests import get
+from threading import Timer
 
 
 class Resources:
@@ -9,3 +10,16 @@ class Resources:
         result = get(self.url)
         result_dict = result.json()
         return result_dict
+
+
+class Rupiah:
+    def __init__(self, url, interval_sec):
+        self.dollar_rate = 0.0
+        self.url = url
+        fetcher_thread = Timer(interval_sec, self.fetch)
+        fetcher_thread.start()
+
+    def fetch(self):
+        result = get(self.url)
+        result_dict = result.json()
+        self.dollar_rate = result_dict['USD_IDR']
