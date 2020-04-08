13c1f0de2b253059899468b33b3569c8700215b0
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-09 03:57:34 +0700

    feat(env): add env module contains all needed function to read env

---
+++ py-simple-app/env.py
@@ -0,0 +1,19 @@
+from os import environ
+
+
+def read_url_rupiah_rate():
+    var_name = 'URL_RUPIAH_RATE'
+    return read(var_name)
+
+
+def read_url_resources():
+    var_name = 'URL_RESOURCES'
+    return read(var_name)
+
+
+def read(var_name):
+    url = environ.get(var_name)
+    if url is None:
+        print('failed to read env var: ', var_name)
+
+    return url
