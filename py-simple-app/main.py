e9c6bdfb2f1373cf4304ba7ded3428bd3664911b
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-09 04:03:12 +0700

    feat(main): register api and add run thread to check rupiah rate

--- py-simple-app/main.py
+++ py-simple-app/main.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from cmd import parse_args
 from api import api
+from data import update_rupiah_cache_every
+from threading import Thread
 
 args = parse_args()
 
 app = Flask(__name__)
+app.register_blueprint(api)
 
 if __name__ == '__main__':
+    rupiah_checker_thread = Thread(target=update_rupiah_cache_every, args=(5,))
+    rupiah_checker_thread.start()
     app.run(port=args.port)
