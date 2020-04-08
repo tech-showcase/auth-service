ff6edbf580fb4da9c51e45cbf29a9fbc79190658
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-09 00:43:34 +0700

    feat(model): inject price_in_dollar in every resource data

--- py-simple-app/model.py
+++ py-simple-app/model.py
@@ -3,12 +3,17 @@ from threading import Timer
 
 
 class Resources:
-    def __init__(self, url):
+    def __init__(self, url, price_converter):
         self.url = url
+        self.price_converter = price_converter
 
     def fetch(self):
         result = get(self.url)
         result_dict = result.json()
+
+        for index in range(len(result_dict)):
+            price = result_dict[index]["price"]
+            result_dict[index]["price_in_dollar"] = self.price_converter.convert_to_dollar(price)
 
         return result_dict
 
@@ -26,4 +31,7 @@ class Rupiah:
         self.dollar_rate = result_dict['USD_IDR']
 
     def convert_to_dollar(self, price_in_rupiah):
-        return self.dollar_rate * price_in_rupiah
+        if price_in_rupiah is None:
+            return None
+        else:
+            return self.dollar_rate * price_in_rupiah
