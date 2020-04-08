4ca1eb689856b233106909cb00d61352f3f0a5d1
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-09 04:10:13 +0700

    fix(main): read interval checking rupiah rate instead of hardcode it

--- py-simple-app/main.py
+++ py-simple-app/main.py
@@ -3,6 +3,7 @@ from cmd import parse_args
 from api import api
 from data import update_rupiah_cache_every
 from threading import Thread
+import env
 
 args = parse_args()
 
@@ -10,6 +11,6 @@ app = Flask(__name__)
 app.register_blueprint(api)
 
 if __name__ == '__main__':
-    rupiah_checker_thread = Thread(target=update_rupiah_cache_every, args=(5,))
+    rupiah_checker_thread = Thread(target=update_rupiah_cache_every, args=(int(env.read_interval_rupiah_rate()),))
     rupiah_checker_thread.start()
     app.run(port=args.port)
