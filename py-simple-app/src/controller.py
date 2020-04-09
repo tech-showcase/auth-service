a08ed0899e12531810a3bcb09a3bbe13ca4a016f
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-09 23:53:19 +0700

    feat(controller): add aggregate price func

--- py-simple-app/src/controller.py
+++ py-simple-app/src/controller.py
@@ -6,3 +6,8 @@ def fetch_resources(resources, price_converter):
         result_dict[index]["price_in_dollar"] = price_converter.convert_to_dollar(price)
 
     return result_dict
+
+
+def aggregate_resources_price(resources):
+    result_dict = resources.aggregate_price()
+    return result_dict
