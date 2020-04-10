10ff8390b47da43a934bb2d042e66c9b9fe111c0
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-10 13:55:56 +0700

    feat(api): add auth middleware

--- py-simple-app/src/api.py
+++ py-simple-app/src/api.py
@@ -3,11 +3,13 @@ import controller
 import env
 from model import Resources
 import data as data
+from middleware import authentication_function, admin_authorization_function
 
 api = Blueprint('api', __name__)
 
 
 @api.route('/api/resources')
+@authentication_function
 def fetch_resources():
     resources = Resources(env.read_url_resources())
     result_dict = controller.fetch_resources(resources, data.rupiah)
@@ -15,6 +17,8 @@ def fetch_resources():
 
 
 @api.route('/api/resources/aggregate_price')
+@authentication_function
+@admin_authorization_function
 def aggregate_resources():
     resources = Resources(env.read_url_resources(), date_str='2020-02-02', province='JAWA TENGAH')
     result_dict = controller.aggregate_resources_price(resources)
