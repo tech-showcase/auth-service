349ded849d799580c2fb5b4deaf9c58d3d04cf79
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-19 23:18:33 +0700

    feat(main): print text at app start

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/tech-showcase/auth-service/api"
 	"github.com/tech-showcase/auth-service/cmd"
 )
 
 func main() {
+	fmt.Println("Hi, I am Auth Service!")
+
 	args := cmd.Parse()
 
 	api.Activate(args.Port)
