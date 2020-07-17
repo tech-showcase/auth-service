9656b49c607c9c1f1128c2649e8c161cb3151d20
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-18 01:00:26 +0700

    feat(controller): add handleAuth and handleToken controller for oauth2

--- controller/oauth2/oauth2.go
+++ controller/oauth2/oauth2.go
@@ -1,13 +1,16 @@
 package oauth2
 
 import (
+	"github.com/tech-showcase/auth-service/global"
 	"net/http"
 )
 
-func Authorize(w http.ResponseWriter, r *http.Request) {
-
+func Authorize(w http.ResponseWriter, r *http.Request) (err error) {
+	err = global.OAuth2Helper.HandleAuthorizeRequest(w, r)
+	return
 }
 
-func Token(w http.ResponseWriter, r *http.Request) {
-
+func Token(w http.ResponseWriter, r *http.Request) (err error) {
+	err = global.OAuth2Helper.HandleTokenRequest(w, r)
+	return
 }
