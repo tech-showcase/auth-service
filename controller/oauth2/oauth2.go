9a73160fdea4bd183bbc2c6ccd852a14611a99f9
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-18 00:40:15 +0700

    feat(presenter): add oauth2 authorize and token

---
+++ controller/oauth2/oauth2.go
@@ -0,0 +1,13 @@
+package oauth2
+
+import (
+	"net/http"
+)
+
+func Authorize(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Token(w http.ResponseWriter, r *http.Request) {
+
+}
--- presenter/oauth2/oauth2.go
+++ presenter/oauth2/oauth2.go
@@ -1,11 +1,14 @@
 package oauth2
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tech-showcase/auth-service/controller/oauth2"
+)
 
 func Authorize(ctx *gin.Context) {
-
+	oauth2.Authorize(ctx.Writer, ctx.Request)
 }
 
 func Token(ctx *gin.Context) {
-
+	oauth2.Token(ctx.Writer, ctx.Request)
 }
