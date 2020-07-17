8e768108c99a78e65e943b97fe25aa804e394fa8
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-18 00:39:51 +0700

    feat(helper): add oauth2 helper

---
+++ helper/oauth2.go
@@ -0,0 +1,52 @@
+package helper
+
+import (
+	"github.com/go-oauth2/oauth2/v4/errors"
+	"github.com/go-oauth2/oauth2/v4/manage"
+	"github.com/go-oauth2/oauth2/v4/models"
+	"github.com/go-oauth2/oauth2/v4/server"
+	"github.com/go-oauth2/oauth2/v4/store"
+	"log"
+)
+
+type (
+	oauth2Helper struct {
+		server *server.Server
+	}
+	OAuth2Helper interface{}
+)
+
+func NewOAuth2Helper() OAuth2Helper {
+	var instance oauth2Helper
+	instance.server = initOAuth2Server()
+
+	return instance
+}
+
+func initOAuth2Server() *server.Server {
+	manager := manage.NewDefaultManager()
+	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	clientStore := store.NewClientStore()
+	clientStore.Set("000000", &models.Client{
+		ID:     "000000",
+		Secret: "999999",
+		Domain: "http://localhost",
+	})
+	manager.MapClientStorage(clientStore)
+
+	srv := server.NewDefaultServer(manager)
+	srv.SetAllowGetAccessRequest(true)
+	srv.SetClientInfoHandler(server.ClientFormHandler)
+
+	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+		log.Println("Internal Error:", err.Error())
+		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
+	})
+
+	return srv
+}
