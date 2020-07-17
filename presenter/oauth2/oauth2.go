64dc0451dff94a64de05c6cf2c99dc568dc81f67
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-18 01:02:40 +0700

    feat(presenter): add error handling for handleAuth and handleToken

--- presenter/oauth2/oauth2.go
+++ presenter/oauth2/oauth2.go
@@ -3,12 +3,19 @@ package oauth2
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/tech-showcase/auth-service/controller/oauth2"
+	"net/http"
 )
 
 func Authorize(ctx *gin.Context) {
-	oauth2.Authorize(ctx.Writer, ctx.Request)
+	err := oauth2.Authorize(ctx.Writer, ctx.Request)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
 }
 
 func Token(ctx *gin.Context) {
-	oauth2.Token(ctx.Writer, ctx.Request)
+	err := oauth2.Token(ctx.Writer, ctx.Request)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
 }
