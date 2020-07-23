5781dedad47b341619a7ccf462766271a4f0b2b6
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-24 01:46:18 +0700

    feat(api): add oauth2 api
    - add login api
    - add authorize api
    - add token api

--- api/oauth2.go
+++ api/oauth2.go
@@ -2,11 +2,19 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	oauth2Middleware "github.com/tech-showcase/auth-service/middleware/oauth2"
 	oauth2Presenter "github.com/tech-showcase/auth-service/presenter/oauth2"
 )
 
 func RegisterOAuth2API(router *gin.Engine) {
-	authRoute := router.Group("/oauth2")
-	authRoute.GET("/authorize", oauth2Presenter.Authorize)
-	authRoute.GET("/token", oauth2Presenter.Token)
+	oauth2Route := router.Group("/oauth2")
+	oauth2Route.GET("/login", oauth2Presenter.GetLogin)
+	oauth2Route.POST("/login", oauth2Presenter.PostLogin)
+	oauth2Route.POST("/token", oauth2Presenter.Token)
+	oauth2Route.Use(oauth2Middleware.SessionDataMiddleware, oauth2Middleware.MustBeLoggedInMiddleware)
+	{
+		oauth2Route.GET("/authorize", oauth2Presenter.Authorize)
+		oauth2Route.POST("/authorize", oauth2Presenter.Authorize)
+		oauth2Route.GET("/authorization", oauth2Presenter.GetAuthorization)
+	}
 }
