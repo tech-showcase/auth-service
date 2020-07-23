40d9145cc1f66ce0f121bacbaf03d784b9a91a41
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-24 01:45:05 +0700

    feat(controller): add oauth2 controller
    - add login controller
    - add authorize controller
    - add token controller

--- controller/oauth2/oauth2.go
+++ controller/oauth2/oauth2.go
@@ -3,14 +3,58 @@ package oauth2
 import (
 	"github.com/tech-showcase/auth-service/helper"
 	"net/http"
+	"net/url"
 )
 
-func Authorize(w http.ResponseWriter, r *http.Request) (err error) {
+func Authorize(w http.ResponseWriter, r *http.Request, sessionData helper.SessionData) (err error) {
+	var form url.Values
+	if v, ok := sessionData.Get("ReturnUri"); ok {
+		form = v.(url.Values)
+	}
+	r.Form = form
+
 	err = helper.OAuth2HelperInstance.HandleAuthorizeRequest(w, r)
+	return
+}
+
+func SaveAuthorizationData(r *http.Request, sessionData helper.SessionData) {
+	_, ok := sessionData.Get("ReturnUri")
+	if !ok {
+		if r.Form == nil {
+			r.ParseForm()
+		}
+		sessionData.Set("ReturnUri", r.Form)
+		sessionData.Save()
+	}
+}
+
+func IsLoggedIn(sessionData helper.SessionData) (isLoggedIn bool) {
+	_, isLoggedIn = sessionData.Get("LoggedInUserID")
+	return
+}
+
+func IsAuthorized(sessionData helper.SessionData) (isAuthorized bool) {
+	_, isAuthorized = sessionData.Get("AuthorizedUserID")
 	return
 }
 
 func Token(w http.ResponseWriter, r *http.Request) (err error) {
 	err = helper.OAuth2HelperInstance.HandleTokenRequest(w, r)
+	return
+}
+
+func PostLogin(r *http.Request, sessionData helper.SessionData) (err error) {
+	if r.Form == nil {
+		if err = r.ParseForm(); err != nil {
+			return
+		}
+	}
+	sessionData.Set("LoggedInUserID", r.Form.Get("username"))
+
+	err = sessionData.Save()
+	if err != nil {
+		return
+	}
+
 	return
 }
