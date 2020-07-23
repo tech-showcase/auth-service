99b86725f61e06a86b8d2ac75fa6b43ee9188f0a
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-24 01:42:08 +0700

    feat(helper): add AuthorizeUser func for getting user id

--- helper/oauth2.go
+++ helper/oauth2.go
@@ -14,10 +14,22 @@ type (
 	oauth2Helper struct {
 		server *server.Server
 	}
-	OAuth2Helper interface{
+	OAuth2Helper interface {
 		HandleAuthorizeRequest(w http.ResponseWriter, r *http.Request) (err error)
 		HandleTokenRequest(w http.ResponseWriter, r *http.Request) (err error)
 	}
+)
+
+const (
+	OAuth2SessionName = "oauth2-session-name"
+
+	BaseOAuth2Url    = "/oauth2"
+	LoginUrl         = BaseOAuth2Url + "/login"
+	AuthorizationUrl = BaseOAuth2Url + "/authorization"
+
+	BaseStaticFilepath  = "static"
+	LoginStaticFilepath = BaseStaticFilepath + "/login.html"
+	AuthStaticFilepath  = BaseStaticFilepath + "/authorization.html"
 )
 
 var OAuth2HelperInstance OAuth2Helper
@@ -35,30 +47,42 @@ func NewOAuth2Helper() OAuth2Helper {
 
 func initOAuth2Server() *server.Server {
 	manager := manage.NewDefaultManager()
+	manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
 	manager.MustTokenStorage(store.NewMemoryTokenStore())
 
 	clientStore := store.NewClientStore()
-	clientStore.Set("000000", &models.Client{
-		ID:     "000000",
-		Secret: "999999",
-		Domain: "http://localhost",
+	clientStore.Set("082222333444", &models.Client{
+		ID:     "082222333444",
+		Secret: "082222333444",
+		Domain: "http://localhost:9094/oauth2",
 	})
 	manager.MapClientStorage(clientStore)
 
-	srv := server.NewDefaultServer(manager)
-	srv.SetAllowGetAccessRequest(true)
-	srv.SetClientInfoHandler(server.ClientFormHandler)
-
+	srv := server.NewServer(server.NewConfig(), manager)
+	srv.SetUserAuthorizationHandler(AuthorizeUser)
 	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
 		log.Println("Internal Error:", err.Error())
 		return
 	})
-
 	srv.SetResponseErrorHandler(func(re *errors.Response) {
 		log.Println("Response Error:", re.Error.Error())
 	})
 
 	return srv
+}
+
+func AuthorizeUser(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+	sessionData, err := NewSessionData(SessionStorageInstance, OAuth2SessionName, r, w)
+	if err != nil {
+		return
+	}
+
+	userIDInf, _ := sessionData.Get("LoggedInUserID")
+	userID = userIDInf.(string)
+	sessionData.Set("AuthorizedUserID", userID)
+	err = sessionData.Save()
+
+	return
 }
 
 func (instance *oauth2Helper) HandleAuthorizeRequest(w http.ResponseWriter, r *http.Request) (err error) {
