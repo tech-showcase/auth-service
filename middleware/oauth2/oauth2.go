239ecca4335ad581a7db4644f7e571039f20cd9d
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-07-24 01:43:25 +0700

    feat(middleware): add oauth2 middleware
    - add middleware for gathering authorize data
    - add middleware for checking if user is already logged in

---
+++ middleware/oauth2/oauth2.go
@@ -0,0 +1,30 @@
+package oauth2
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tech-showcase/auth-service/controller/oauth2"
+	"github.com/tech-showcase/auth-service/helper"
+	"net/http"
+)
+
+func SessionDataMiddleware(ctx *gin.Context) {
+	sessionData, err := helper.NewSessionData(helper.SessionStorageInstance, helper.OAuth2SessionName, ctx.Request, ctx.Writer)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+	}
+
+	oauth2.SaveAuthorizationData(ctx.Request, sessionData)
+	ctx.Next()
+}
+
+func MustBeLoggedInMiddleware(ctx *gin.Context) {
+	sessionData, err := helper.NewSessionData(helper.SessionStorageInstance, helper.OAuth2SessionName, ctx.Request, ctx.Writer)
+	if err != nil {
+		ctx.AbortWithError(http.StatusInternalServerError, err)
+	}
+
+	if !oauth2.IsLoggedIn(sessionData) {
+		ctx.Redirect(http.StatusFound, helper.LoginUrl)
+		ctx.Abort()
+	}
+}
